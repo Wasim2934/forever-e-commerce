4c7b07b69905e7ee60d682001bd817c18f080a12
Wasim2934 <mdwasimlesnar0019@gmail.com>
2026-07-04 02:04:59 +0530

    productImage

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { shopContext } from '../context/ShopContext';
 
 const Product = () => {
 
   const { productId } = useParams();  
+  const { products } = useContext(shopContext)
+  const [ productData, setProductData ] = useState(false)
+  const [ image, setImage ] = useState('');
 
-  const [value, setValue] = useState();
+const fetchProductData = async () => {
 
-  return (
-    <div>
+  products.map((item) => {
+    if (item._id === productId) {
+      setProductData(item);
+      console.log(item);
+      setImage(item.image[0]);
+      return null;
+    }
+  })
+
+}
+
+useEffect(() => {
+  fetchProductData()
+}, [productId, products])
+
+  return productData ? (
+    <div className=''>
+
+    </div>
+  ) : (
+    <div className='opacity-0'>
 
     </div>
   )
