50ef389a6a5f02fae16d8836f7137590deeb0c41
Wasim2934 <mdwasimlesnar0019@gmail.com>
2026-07-05 10:03:05 +0530

    product page completed

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,8 +1,9 @@
-import { createContext, useState } from "react"
+import { createContext, useEffect, useState } from "react"
 import { products } from '../assets/assets'
+import { toast } from "react-toastify";
 
 export const shopContext = createContext()
-
+ 
 const ShopContextProvider = ({children}) => {
 
   const currency = '$';
@@ -12,13 +13,47 @@ const ShopContextProvider = ({children}) => {
   const [showSearch, setShowSearch] = useState(false);
   const [cartItems, setCartItems] = useState({});
 
-  
+  const addToCart = async (itemId, size) => {
+    
+    if (!size) {
+      toast.error('Select product size');
+      return; 
+    }
+
+    let cartData = structuredClone(cartItems);
+
+    if (cartData[itemId]) {
+      if (cartData[itemId][size]) {
+        cartData[itemId][size] += 1;
+      } else {
+        cartData[itemId][size] = 1;
+      }
+    } else {
+      cartData[itemId] = {};
+      cartData[itemId][size] = 1;
+    }
+    setCartItems(cartData);
+  }
+
+  const getCartCount = () => {
+    let totalCount = 0;
+
+    for (let items in cartItems) {
+      for (let item in cartItems[items]) {
+        if (cartItems[items][item] > 0) {
+          totalCount += cartItems[items][item]
+        }
+      }
+    }
+    return totalCount;
+  }
 
   const value = {
     products,
     currency,
     delivery_fee,
-    search, setSearch, showSearch, setShowSearch
+    search, setSearch, showSearch, setShowSearch,
+    cartItems, addToCart, getCartCount
   }
 
   return (
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -7,7 +7,7 @@ import RelatedProducts from '../components/RelatedProducts';
 const Product = () => {
 
   const { productId } = useParams();  
-  const { products, currency } = useContext(shopContext)
+  const { products, currency, addToCart } = useContext(shopContext)
   const [ productData, setProductData ] = useState(false)
   const [ image, setImage ] = useState('');
   const [ size, setSize ] = useState('');
@@ -74,7 +74,7 @@ useEffect(() => {
                     })}
                   </div>
                 </div>
-                <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+                <button onClick={() => addToCart(productData._id, size)} className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
                 <hr className='mt-8 sm:w-4/5 border-gray-300' />
                 <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
                     <p>100% Original product.</p>
